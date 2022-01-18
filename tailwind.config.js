eca52bc0aab50a3556480b7380cf07662a6271b2
Nyasha D. Chiroro <chirorodennis@gmail.com>
2022-01-18 10:26:23 +0200

    Added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
     content: ['./index.html'],
     theme: {
-        extend: {},
+        colors: {
+            white: 'hsl(277, 64%, 61%)',
+            blue: {
+                1: 'hsl(233, 47%, 7%)',
+                2: 'hsl(244, 38%, 16%)',
+            },
+            violet: 'hsl(277, 64%, 61%)',
+            gray: {
+                1: 'hsla(0, 0%, 100%, 0.75)',
+                2: 'hsla(0, 0%, 100%, 0.6)',
+            },
+        },
     },
     plugins: [],
 }
